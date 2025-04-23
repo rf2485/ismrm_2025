6a92bbd32826c83aac8f62c12e65f3529bcd7981
Ryn Flaherty <rf2485@nyu.edu>
2025-04-23 15:20:37 -0400

    update projectdir in amyco_noddi.py, add branching logic in process_dwi to skip steps already completed.

--- amico_noddi.py
+++ amico_noddi.py
@@ -2,7 +2,7 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import amico
 import sys
-study="/gpfs/data/lazarlab/CamCan995/derivatives/designer_tbss/dwi_processed/"
+study="/gpfs/data/lazarlab/CamCan995/derivatives/scd/ismrm_2025/dwi_processed/"
 subject=sys.argv[1]
 amico.core.setup()
 ae = amico.Evaluation(study, subject)
