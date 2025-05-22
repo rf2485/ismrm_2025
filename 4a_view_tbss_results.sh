54a15b7da43f6d6cc7aaf3dbca6130f93a3f779e
Ryn Flaherty <rf2485@nyu.edu>
2025-05-22 16:16:42 -0400

    amico_noddi script had wrong project directory. Updated view_tbss_results to include viewing the correlations with memory.

--- 4a_view_tbss_results.sh
+++ 4a_view_tbss_results.sh
@@ -12,3 +12,20 @@ cd $projectdir/tbss/stats
 for metric in "${metric_list[@]}"; do
 	fsleyes -std1mm all_${metric} mean_FA_skeleton_mask -cm green ${metric}_clusterm_corrp_tstat1 -cm blue -dr 0.949 1 ${metric}_clusterm_corrp_tstat2 -cm red -dr 0.949 1
 done
+
+#no covariates
+cd $projectdir/tbss/stats
+for metric in "${metric_list[@]}"; do
+	fsleyes -std1mm all_${metric} mean_FA_skeleton_mask -cm green ${metric}_clusterm_corrp_tstat1 -cm blue ${metric}_clusterm_corrp_tstat2 -cm red #red means higher in SCD (tstat2)
+done
+
+covariate_list=(age_int anxiety_int bmi_int depression_int memory_int)
+for cov in "${covariate_list[@]}"; do
+	for metric in "${metric_list[@]}"; do
+		fsleyes -std1mm mean_FA_skeleton_mask -cm green ${cov}/${cov}_${metric}_clusterm_corrp_tstat1 -cm blue ${cov}/${cov}_${metric}_clusterm_corrp_tstat2 -cm red #red means higher in SCD (tstat2)
+		fsleyes -std1mm mean_FA_skeleton_mask -cm green ${cov}/${cov}_${metric}_clusterm_corrp_tstat3 -cm blue ${cov}/${cov}_${metric}_clusterm_corrp_tstat4 -cm red #red means SCD has a steeper slope than controls (tstat4)
+		fsleyes -std1mm mean_FA_skeleton_mask -cm green ${cov}/${cov}_${metric}_clusterm_corrp_tstat5 -cm red ${cov}/${cov}_${metric}_clusterm_corrp_tstat6 -cm blue #red means positive slope across groups
+		fsleyes -std1mm mean_FA_skeleton_mask -cm green ${cov}/${cov}_${metric}_clusterm_corrp_tstat7 -cm red ${cov}/${cov}_${metric}_clusterm_corrp_tstat8 -cm blue #red means positive slope in controls
+		fsleyes -std1mm mean_FA_skeleton_mask -cm green ${cov}/${cov}_${metric}_clusterm_corrp_tstat9 -cm red ${cov}/${cov}_${metric}_clusterm_corrp_tstat10 -cm blue #red means positive slope in SCD
+	done
+done
